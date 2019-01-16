e9a5a401c6eb48d4b19c320bdb4ce8d9c8173ba9
shlommi <shlommi@gmail.com>
2019-01-16 16:58:40 +0200

    Updates

---
+++ precache-manifest.baadc858d48843c76ebe5c81f911e1c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67122e2cdde974b7bf57",
+    "url": "/Random-Quote-Machine/static/js/runtime~main.67122e2c.js"
+  },
+  {
+    "revision": "8ada324a4750c08aa35d",
+    "url": "/Random-Quote-Machine/static/js/main.8ada324a.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/Random-Quote-Machine/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "8ada324a4750c08aa35d",
+    "url": "/Random-Quote-Machine/static/css/main.5cbd21e1.chunk.css"
+  },
+  {
+    "revision": "fb3b145b8a01cf718921dcb5fbf1618a",
+    "url": "/Random-Quote-Machine/index.html"
+  }
+];
